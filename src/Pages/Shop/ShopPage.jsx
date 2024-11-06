e896587e7cd86190892adaa689363f1985b2edf3
nasrin137 <nasrinakter.hrm@gmail.com>
2024-11-06 19:45:40 +0800

    added filter section

--- src/Pages/Shop/ShopPage.jsx
+++ src/Pages/Shop/ShopPage.jsx
@@ -1,6 +1,56 @@
-
+import { useEffect, useState } from 'react'
+import productsData from '../../data/products.json'
+import ProductCards from './ProductCards'
+import ShopFiltering from './ShopFiltering'
+const filters = {
+    categories:['all','accessories','dress','jewellery','cosmetics'],
+    colors:['all','black','red','gold','blue','silver','beige','green'],
+    priceRanges:[
+        {label:'Under $50',min:0,max:50},
+        {label:'$50 - 100',min:50,max:100},
+        {label:'$100 - $200',min:100,max:200},
+        {label:'$200 and above',min:200,max:Infinity}
+    ]
+}
 
 const ShopPage = () => {
+    const[products,setProducts] = useState(productsData)
+    const[filtersState,setFiltersState] = useState({
+        category:'all',
+        color:'all',
+        priceRange:''
+    })
+
+    // filter function
+    const applyFilters = ()=>{
+        let filteredProducts = productsData
+        // filter by category
+        if(filtersState.category && filtersState.category !=='all'){
+            filteredProducts = filteredProducts.filter((product)=>product.category === filtersState.category)
+        }
+        // filter by color
+        if(filtersState.color && filtersState.color !=='all'){
+            filteredProducts = filteredProducts.filter((product)=>product.color === filtersState.color)
+        }
+        // filter by priceRange
+        if(filtersState.priceRange){
+            const[minPrice,maxPrice] = filtersState.priceRange.split('-').map(Number)
+            filteredProducts = filteredProducts.filter((product)=>product.price >=minPrice && product.price <=maxPrice)
+        }
+        setProducts(filteredProducts)
+    }
+    useEffect(()=>{
+        applyFilters()
+    },[filtersState])
+
+    // clear the filters
+    const clearFilters = ()=>{
+        setFiltersState({
+            category:'all',
+            color:'all',
+            priceRange:''
+        })
+    }
   return (
     <>
      <section className="section__container bg-primary-light">
@@ -10,10 +60,11 @@ const ShopPage = () => {
     <section className="section__container">
         <div className="flex flex-col md:flex-row md:gap-12 gap-8">
             {/* left side */}
-            <div>Shop Filtering</div>
+            <ShopFiltering filters={filters} filtersState={filtersState} setFiltersState={setFiltersState} clearFilters={clearFilters}></ShopFiltering>
             {/* right side */}
             <div>
-                <h3 className="text-xl font-medium mb-4">Products Available</h3>
+                <h3 className="text-xl font-medium mb-4">Products Available:{products.length}</h3>
+                <ProductCards products={products}></ProductCards>
             </div>
         </div>
     </section>
